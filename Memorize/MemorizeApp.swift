436d21aebb841f1884bc0be3f829c70a73e62174
kiemhvs <kiemhvs@hybrid-technologies.vn>
2024-10-13 17:07:32 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ha Vu Son Kiem on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
